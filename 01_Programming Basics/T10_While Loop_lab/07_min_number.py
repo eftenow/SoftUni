b8b9a21e19179e910aac7ffeba71e876400adf4a
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-06-08 16:27:04 +0300

    Create 07_min_number.py

---
+++ 01_Programming Basics/T10_While Loop_lab/07_min_number.py
@@ -0,0 +1,13 @@
+import sys
+
+n = input()
+biggest_number = sys.maxsize
+current_number = 0
+while n != "Stop":
+    number = int(n)
+    if biggest_number > number:
+        biggest_number = number
+
+    n = input()
+
+print(biggest_number)
