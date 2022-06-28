768719ebdc07d4a363876efed98dec08d6195229
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-06-28 23:36:41 +0300

    Delete 02_Programming Fundamentals/00_mid_exam_prep/08_lists_basics_exercise directory

--- 02_Programming Fundamentals/00_mid_exam_prep/08_lists_basics_exercise/01_invert_values.py
+++
@@ -1,7 +0,0 @@
-numbers = input()
-numbers_list = numbers.split()
-new_list = []
-for i in range(len(numbers_list)):
-    new_number = int(numbers_list[i]) * (-1)
-    new_list.append(new_number)
-print(new_list)
