49559ea4e0fed1033b4a4d069d74777200ec19bb
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-07-25 18:02:10 +0300

    Create 06_triples_of_latin_letters.py

---
+++ 02_Programming Fundamentals/05_data_types_and_variables_exercise/06_triples_of_latin_letters.py
@@ -0,0 +1,6 @@
+amount_of_letters = int(input()) # 2
+starting_letter = 97
+for a in range (starting_letter, starting_letter + amount_of_letters):
+    for b in range(starting_letter, starting_letter + amount_of_letters):
+        for c in range(starting_letter, starting_letter + amount_of_letters):
+            print(chr(a) + chr(b) + chr(c))
