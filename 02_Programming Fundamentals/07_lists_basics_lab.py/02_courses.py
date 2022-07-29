5923354b383c7031ecc086371ebc69db67d651f0
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-07-29 07:44:17 +0300

    Create 02_courses.py

---
+++ 02_Programming Fundamentals/07_lists_basics_lab.py/02_courses.py
@@ -0,0 +1,7 @@
+number_of_courses = int(input())
+list_of_courses = []
+for course in range(number_of_courses):
+    current_course = input()
+    list_of_courses.append(current_course)
+
+print(list_of_courses)
