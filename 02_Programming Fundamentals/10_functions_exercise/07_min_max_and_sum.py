ec3a1d2475e557600bf4f2a7e0f216b97e66374a
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-08-05 19:23:33 +0300

    Create 07_min_max_and_sum.py

---
+++ 02_Programming Fundamentals/10_functions_exercise/07_min_max_and_sum.py
@@ -0,0 +1,9 @@
+numbers = list(map(int, input().split()))
+
+min = min(numbers)
+max = max(numbers)
+sum = sum(numbers)
+
+print(f"The minimum number is {min}")
+print(f"The maximum number is {max}")
+print(f"The sum number is: {sum}")
