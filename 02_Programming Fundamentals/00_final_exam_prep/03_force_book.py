c85f280478cc867f35f90a0285267037b166fbd2
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-08-06 08:01:57 +0300

    Create 03_force_book.py

---
+++ 02_Programming Fundamentals/00_final_exam_prep/03_force_book.py
@@ -0,0 +1,43 @@
+def set_side(to_side, to_user, both_sides):
+    for key, values in both_sides.items():
+        if to_user in values:
+            return both_sides
+    if to_side not in both_sides:
+        both_sides[to_side] = [to_user]
+    else:
+        both_sides[to_side].append(to_user)
+    return both_sides
+
+
+def change_side(to_user, to_side, both_sides):
+    for key, values in both_sides.items():
+        if to_user in values:
+            if to_side not in both_sides:
+                both_sides[to_side] = [to_user]
+            else:
+                both_sides[to_side].append(to_user)
+            both_sides[key].remove(to_user)
+            return both_sides
+    else:
+        return set_side(to_side, to_user, both_sides)
+
+
+user_side = input()
+sides = {}
+
+while user_side != "Lumpawaroo":
+    if " | " in user_side:
+        side, user = user_side.split(" | ")
+        sides = set_side(side, user, sides)
+
+    elif " -> " in user_side:
+        user, side = user_side.split(" -> ")
+        sides = change_side(user, side, sides)
+        print(f"{user} joins the {side} side!")
+    user_side = input()
+
+for key, values in sides.items():
+    if len(values) > 0:
+        print(f"Side: {key}, Members: {len(values)}")
+        for value in values:
+            print(f"! {value}")
