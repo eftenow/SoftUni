522a059e903a4c1e633ec887b0822c1c25cb2e35
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-08-12 18:54:00 +0300

    Update 01_which_are_in.py

--- 02_Programming Fundamentals/12_lists_advanced_exercise/01_which_are_in.py
+++ 02_Programming Fundamentals/12_lists_advanced_exercise/01_which_are_in.py
@@ -1,10 +1,8 @@
 substrings = input().split(", ")
-words = input().split(", ")
+words = input()
 list_of_matches = []
 for substring in substrings:
-    for word in words:
-        if substring in word:
-            list_of_matches.append(substring)
-            break
+    if substring in words:
+        list_of_matches.append(substring)
 
 print(list_of_matches)
