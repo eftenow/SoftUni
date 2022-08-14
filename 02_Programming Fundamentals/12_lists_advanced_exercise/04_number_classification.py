dfa762146acc1a7736cbaff0adb3b7dc553a2567
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-08-14 20:22:05 +0300

    Create 04_number_classification.py

---
+++ 02_Programming Fundamentals/12_lists_advanced_exercise/04_number_classification.py
@@ -0,0 +1,12 @@
+numbers = input().split(", ")
+numbers = list(map(int, numbers))
+
+even_numbers = [str(x) for x in numbers if x % 2 == 0]
+odd_numbers = [str(x) for x in numbers if x % 2 != 0]
+positive_numbers = [str(x) for x in numbers if x >= 0]
+negative_numbers = [str(x) for x in numbers if x < 0]
+
+print(f"Positive: {', '.join(positive_numbers)}" )
+print(f"Negative: {', '.join(negative_numbers)}" )
+print(f"Even: {', '.join(even_numbers)}" )
+print(f"Odd: {', '.join(odd_numbers)}" )
