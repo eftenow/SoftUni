f4affec242c0684328ad07359473c722d097561d
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-10-25 10:25:36 +0300

    Create 01_diagonals.py

---
+++ 03_Python Advanced/07_multidimensional_lists_exercise/01_diagonals.py
@@ -0,0 +1,20 @@
+n = int(input())
+matrix = []
+
+primary_diagonal = []
+secondary_diagonal = []
+
+for _ in range(n):
+    matrix.append([int(x) for x in input().split(', ')])
+
+for row_ind in range(n):
+    number = matrix[row_ind][row_ind]
+    primary_diagonal.append(number)
+
+for row_ind in range(n):
+    col_ind = (n - 1) - row_ind
+    number = matrix[row_ind][col_ind]
+    secondary_diagonal.append(number)
+
+print(f"Primary diagonal: {', '.join(str(x) for x in primary_diagonal)}. Sum: {sum(primary_diagonal)}")
+print(f"Secondary diagonal: {', '.join(str(x) for x in secondary_diagonal)}. Sum: {sum(secondary_diagonal)}")
