c244a1c2f68eafec46c1acb02ea59dbd54cc32c6
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-11-09 14:23:01 +0200

    Add files via upload

---
+++ 04_Python OOP/02_first_steps_in_oop_exercise/08_pokemon_battle/project/trainer.py
@@ -0,0 +1,27 @@
+from pokemon import Pokemon
+
+class Trainer:
+    def __init__(self, name):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if any([x.name == pokemon.name for x in self.pokemons]):
+            return "This pokemon is already caught"
+        self.pokemons.append(pokemon)
+        return f'Caught {pokemon.pokemon_details()}'
+
+    def release_pokemon(self, pokemon_name):
+        for pokemon in self.pokemons:
+            if pokemon.name == pokemon_name:
+                self.pokemons.remove(pokemon)
+                return f'You have released {pokemon_name}'
+        else:
+            return 'Pokemon is not caught'
+
+    def trainer_data(self):
+        result = f'Pokemon Trainer {self.name}\nPokemon count {len(self.pokemons)}'
+        for p in self.pokemons:
+            result += '\n'f'- {p.pokemon_details()}'
+        return result
+
