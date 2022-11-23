13602d67cd3d8139268b02f880707fc40980f5a3
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-11-23 14:27:13 +0200

    Create 01_person.py

---
+++ 04_Python OOP/07_encapsulation_lab/01_person.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, __name, __age):
+        self.__name = __name
+        self.__age = __age
+
+    def get_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+
