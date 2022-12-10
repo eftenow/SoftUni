3044a3563997353f0207f020d6f91547e105f7bd
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-12-10 20:47:10 +0200

    Create 05_take_halves.py

---
+++ 04_Python OOP/14_iterators_and_generators_exercise/05_take_halves.py
@@ -0,0 +1,16 @@
+def solution():
+    def integers():
+        i = 1
+        while True:
+            yield i
+            i += 1
+
+    def halves():
+        for i in integers():
+            yield i / 2
+
+    def take(n, seq):
+        return [next(seq) for _ in range(n)]
+
+
+    return(take, halves, integers)
