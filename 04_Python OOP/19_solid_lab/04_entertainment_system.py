4f619479704ad4a8421efef0664c5a00fef60028
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-12-21 20:27:02 +0200

    Add files via upload

---
+++ 04_Python OOP/19_solid_lab/04_entertainment_system.py
@@ -0,0 +1,55 @@
+class HDMI:
+    def connect_to_device_via_hdmi_cable(self, device): pass
+
+
+class RCA:
+    def connect_to_device_via_rca_cable(self, device): pass
+
+
+class EthernetCable:
+    def connect_to_device_via_ethernet_cable(self, device): pass
+
+
+class PowerOutlet:
+    def connect_device_to_power_outlet(self, device): pass
+
+
+class Television(RCA, HDMI, PowerOutlet):
+    def connect_to_dvd(self, dvd_player):
+        self.connect_to_device_via_rca_cable(dvd_player)
+
+    def connect_to_game_console(self, game_console):
+        self.connect_to_device_via_hdmi_cable(game_console)
+
+    def plug_in_power(self):
+        self.connect_device_to_power_outlet(self)
+
+
+class DVDPlayer(HMI, PowerOutlet):
+    def connect_to_tv(self, television):
+        self.connect_to_device_via_hdmi_cable(television)
+
+    def plug_in_power(self):
+        self.connect_device_to_power_outlet(self)
+
+
+class GameConsole(HDMI, EthernetCable, PowerOutlet):
+    def connect_to_tv(self, television):
+        self.connect_to_device_via_hdmi_cable(television)
+
+    def connect_to_router(self, router):
+        self.connect_to_device_via_ethernet_cable(router)
+
+    def plug_in_power(self):
+        self.connect_device_to_power_outlet(self)
+
+
+class Router(EthernetCable, Television, GameConsole):
+    def connect_to_tv(self, television):
+        self.connect_to_device_via_ethernet_cable(television)
+
+    def connect_to_game_console(self, game_console):
+        self.connect_to_device_via_ethernet_cable(game_console)
+
+    def plug_in_power(self):
+        self.connect_device_to_power_outlet(self)
