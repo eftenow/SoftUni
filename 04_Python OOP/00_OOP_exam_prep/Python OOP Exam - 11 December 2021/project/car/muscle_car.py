9fc1bacb8ca25aba724b411f3b28ca3cdfbc31be
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2022-12-30 10:25:19 +0200

    Delete 04_Python OOP/00_OOP_exam_prep/Python OOP Exam - 11 December 2021/project directory

--- 04_Python OOP/00_OOP_exam_prep/Python OOP Exam - 11 December 2021/project/car/muscle_car.py
+++
@@ -1,9 +0,0 @@
-from project.car.car import Car
-
-
-class MuscleCar(Car):
-    MINIMUM = 250
-    MAXIMUM = 450
-
-    def __init__(self, model, speed_limit):
-        super().__init__(model, speed_limit)
--- 04_Python OOP/00_OOP_exam_prep/Python OOP Exam - 11 December 2021/project/car/sports_car.py
+++
@@ -1,9 +0,0 @@
-from project.car.car import Car
-
-
-class SportsCar(Car):
-    MINIMUM = 400
-    MAXIMUM = 600
-
-    def __init__(self, model, speed_limit):
-        super().__init__(model, speed_limit)
