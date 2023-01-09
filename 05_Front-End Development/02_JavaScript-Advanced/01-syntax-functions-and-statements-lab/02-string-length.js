29b87be9a5cc6d7f7a732ffab1c81bbc3631a892
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-01-09 19:12:08 +0200

    Add files via upload

---
+++ 05_Front-End Development/02_JavaScript-Advanced/01-syntax-functions-and-statements-lab/02-string-length.js
@@ -0,0 +1,9 @@
+function strLength(first, second, third){
+    totalCharacters = first.length + second.length + third.length;
+    avgCharacters = Math.floor(totalCharacters / 3);
+    console.log(totalCharacters);
+    console.log(avgCharacters);
+    
+}
+
+strLength('chocolate', 'ice cream', 'cake')
