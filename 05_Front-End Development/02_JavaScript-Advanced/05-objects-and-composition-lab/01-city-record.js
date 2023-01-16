46366f2eaf522daaee6b1d28597e71e6cd613bd4
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-01-16 08:26:01 +0200

    Create 01-city-record.js

---
+++ 05_Front-End Development/02_JavaScript-Advanced/05-objects-and-composition-lab/01-city-record.js
@@ -0,0 +1,12 @@
+function solve(name, population, treasury){
+    let cityRecord = {
+        name,
+        population,
+        treasury
+    };
+    return cityRecord;
+}
+
+console.log(solve('Tortuga',
+7000,
+15000));
