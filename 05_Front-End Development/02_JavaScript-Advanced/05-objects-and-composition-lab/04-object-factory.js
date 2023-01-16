83b549733c8e6b8877239dffb29d2ff3917ddf4d
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-01-16 08:27:06 +0200

    Add files via upload

---
+++ 05_Front-End Development/02_JavaScript-Advanced/05-objects-and-composition-lab/04-object-factory.js
@@ -0,0 +1,43 @@
+function factory(library, orders){
+    let fulfilledOrders = [];
+    for (let order of orders) {
+        let product = order.template;
+        for (const part of order.parts) {
+            product[part] = library[part];
+        }
+        fulfilledOrders.push(product);
+    }
+    return fulfilledOrders;
+}
+
+
+const library = {
+    print: function () {
+      console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+      console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+      console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+  };
+  const orders = [
+    {
+      template: { name: 'ACME Printer'},
+      parts: ['print']      
+    },
+    {
+      template: { name: 'Initech Scanner'},
+      parts: ['scan']      
+    },
+    {
+      template: { name: 'ComTron Copier'},
+      parts: ['scan', 'print']      
+    },
+    {
+      template: { name: 'BoomBox Stereo'},
+      parts: ['play']      
+    }
+  ];
+  const products = factory(library, orders);
