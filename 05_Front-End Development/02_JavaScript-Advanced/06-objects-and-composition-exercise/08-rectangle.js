372c4e4413d956f40d064c5e6a170617a780060f
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-01-17 08:35:05 +0200

    Add files via upload

---
+++ 05_Front-End Development/02_JavaScript-Advanced/06-objects-and-composition-exercise/08-rectangle.js
@@ -0,0 +1,19 @@
+function rectangle(width, height, color){
+    color = color.charAt(0).toUpperCase() + color.slice(1);
+    let outputRect = {
+        width,
+        height,
+        color,
+        calcArea: () => {return width * height}
+    };
+
+    return outputRect;
+
+}
+
+
+let rect = rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
