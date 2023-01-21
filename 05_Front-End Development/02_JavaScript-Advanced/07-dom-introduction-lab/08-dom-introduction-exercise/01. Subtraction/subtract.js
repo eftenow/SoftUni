8b70b53a4dd2d89e684729765f8bf25e3596b433
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-01-21 10:29:12 +0200

    Delete 05_Front-End Development/02_JavaScript-Advanced/07-dom-introduction-lab/08-dom-introduction-exercise/01. Subtraction directory

--- 05_Front-End Development/02_JavaScript-Advanced/07-dom-introduction-lab/08-dom-introduction-exercise/01. Subtraction/subtract.js
+++
@@ -1,6 +0,0 @@
-function subtract() {
-    let firstNumber = document.getElementById('firstNumber').value;
-    let secondNumber = document.getElementById('secondNumber').value;
-    let result = Number(firstNumber) - Number(secondNumber);
-    document.getElementById('result').textContent = result;
-}
