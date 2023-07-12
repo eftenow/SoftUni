da349420da57ca94b6f95966f79370e40bfc8902
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-07-12 20:02:04 +0300

    Delete 07_Python_Web/02_Templates_Django/templates_demos directory

--- 07_Python_Web/02_Templates_Django/templates_demos/templates_demos/web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-
-from templates_demos.web.views import index, redirect_to_home
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('redirect-home/', redirect_to_home, name='redirect_home' )
-]
