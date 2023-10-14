0cd801b5f60ff0f32dc502f64b10f75c5472a8ef
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-10-14 18:34:49 +0300

    Delete 08_ReactJS/02_components_deep_dive directory

--- 08_ReactJS/02_components_deep_dive/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = (props) => {
-    return (
-        <footer className="footer">
-          <p>Copyright © designed by Mihail Valkov</p>
-        </footer>
-    )
-}
-
-export default Footer;
